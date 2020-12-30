bd62ee2ea452e7a158911c4b505d57470892fbc0
abel <acervantes@shotl.com>
2020-12-30 18:06:30 +0100

    add all copy methods and benchmarks

--- main.go
+++ main.go
@@ -1,14 +1,66 @@
 package main
 
+import "fmt"
+
 type Node struct {
-	id    string
-	value int
+	id       int
+	slice    []int
+	children []Node
 }
 
-func (n Node) copy() Node {
+func (n Node) DeepCopy() Node {
+	sl := len(n.slice)
+	scp := make([]int, sl, sl)
+	copy(scp, n.slice)
+	n.slice = scp
+
+	nl := len(n.children)
+	ccp := make([]Node, nl, nl)
+	for i := range n.children {
+		ccp[i] = n.children[i].DeepCopy()
+	}
+	n.children = ccp
+
 	return n
 }
 
+func (n *Node) DeepCopyPointerReceiver() Node {
+	sl := len(n.slice)
+	scp := make([]int, sl, sl)
+	copy(scp, n.slice)
+
+	nl := len(n.children)
+	ccp := make([]Node, nl, nl)
+	for i := range n.children {
+		ccp[i] = n.children[i].DeepCopy()
+	}
+
+	return Node{
+		id:       n.id,
+		slice:    scp,
+		children: ccp,
+	}
+}
+
+func (n *Node) DeepCopyPointer() *Node {
+	sl := len(n.slice)
+	scp := make([]int, sl, sl)
+	copy(scp, n.slice)
+
+	nl := len(n.children)
+	ccp := make([]Node, nl, nl)
+	for i := range n.children {
+		ccp[i] = n.children[i].DeepCopy()
+	}
+
+	return &Node{
+		id:       n.id,
+		slice:    scp,
+		children: ccp,
+	}
+}
+
+// values
 func LoopCopy(s []Node) []Node {
 	cp := make([]Node, 0)
 	for _, n := range s {
@@ -17,8 +69,8 @@ func LoopCopy(s []Node) []Node {
 	return cp
 }
 
-func LoopCopyPre(s []Node) []Node {
-	cp := make([]Node, len(s))
+func LoopCopyPreLen(s []Node) []Node {
+	cp := make([]Node, len(s)) // zeroed
 	for i, n := range s {
 		cp[i] = n
 	}
@@ -27,6 +79,15 @@ func LoopCopyPre(s []Node) []Node {
 
 func LoopCopyPreCap(s []Node) []Node {
 	cp := make([]Node, 0, len(s))
+	for _, n := range s {
+		cp = append(cp, n)
+	}
+	return cp
+}
+
+func LoopCopyPreLenCap(s []Node) []Node {
+	l := len(s)
+	cp := make([]Node, l, l)
 	for i, n := range s {
 		cp[i] = n
 	}
@@ -34,7 +95,14 @@ func LoopCopyPreCap(s []Node) []Node {
 }
 
 func Copy(s []Node) []Node {
-	cp := make([]Node, len(s))
+	cp := make([]Node, len(s)) // zeroed
+	copy(cp, s)
+	return cp
+}
+
+func CopyPreLenCap(s []Node) []Node {
+	l := len(s)
+	cp := make([]Node, l, l)
 	copy(cp, s)
 	return cp
 }
@@ -43,21 +111,116 @@ func Append(s []Node) []Node {
 	return append(make([]Node, 0, len(s)), s...)
 }
 
-func main() {
+func LoopDeepCopy(s []Node) []Node {
+	l := len(s)
+	cp := make([]Node, l , l)
+	for i := range s {
+		cp[i] = s[i].DeepCopy()
+	}
+	return cp
 }
 
+func LoopDeepCopyPointerReceiver(s []Node) []Node {
+	l := len(s)
+	cp := make([]Node, l , l)
+	for i := range s {
+		cp[i] = s[i].DeepCopyPointerReceiver()
+	}
+	return cp
+}
+
+
+// pointers
 func LoopCopyPointer(s []*Node) []*Node {
 	cp := make([]*Node, 0)
-	for _, n := range s {
-		cp = append(cp, n)
+	for i := range s {
+		cp = append(cp, &Node{
+			id:       s[i].id,
+			slice:    s[i].slice,
+			children: s[i].children,
+		})
 	}
 	return cp
 }
 
-func LoopCopyPrePointer(s []*Node) []*Node {
+func LoopCopyPreLenPointer(s []*Node) []*Node {
 	cp := make([]*Node, len(s))
-	for i, n := range s {
-		cp[i] = n
+	for i := range s {
+		cp[i] = &Node{
+			id:    s[i].id,
+			slice: s[i].slice,
+			children: s[i].children,
+		}
 	}
 	return cp
+}
+
+func LoopCopyPreCapPointer(s []*Node) []*Node {
+	cp := make([]*Node, 0, len(s))
+	for i := range s {
+		cp = append(cp, &Node{
+			id:    s[i].id,
+			slice: s[i].slice,
+			children: s[i].children,
+		})
+	}
+	return cp
+}
+
+func LoopCopyPreLenCapPointer(s []*Node) []*Node {
+	l := len(s)
+	cp := make([]*Node, l, l)
+	for i := range s {
+		cp[i] = &Node{
+			id:    s[i].id,
+			slice: s[i].slice,
+			children: s[i].children,
+		}
+	}
+	return cp
+}
+
+func LoopDeepCopyPointer(s []*Node) []*Node {
+	l := len(s)
+	cp := make([]*Node, l , l)
+	for i := range s {
+		cp[i] = s[i].DeepCopyPointer()
+	}
+	return cp
+}
+
+
+func main() {
+	n := Node{
+		id:     1,
+		slice:  []int{1, 2, 3},
+		children: []Node{
+			{
+				id:     11,
+				slice:  []int{11,12,13},
+			},
+			{
+				id:     22,
+				slice:  []int{22,23,24},
+			},
+		},
+	}
+
+	//cp := n
+	cp := n.DeepCopy()
+
+	cp.id = 10
+	cp.slice[0] = 11
+	cp.children[0].id = 111
+	cp.children[0].slice[0] = 99
+
+	//cp := &Node{
+	//	id:    n.id,
+	//	value: n.value,
+	//	slice: n.slice,
+	//}
+	//cp.slice = append(cp.slice, 4)
+
+	fmt.Println(n)
+	fmt.Println(cp)
 }
--- main_test.go
+++ main_test.go
@@ -1,20 +1,76 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
+	"reflect"
+	"runtime"
 	"testing"
 )
 
-func provideNodes() []Node {
-	ns := make([]Node, 10)
+func provideNodes() (ns []Node) {
+	ns = make([]Node, 10)
 	for i := 0; i < 10; i++ {
+		children := make([]Node, 5)
+		for c := range children {
+			children[c] = Node{
+				id:    i,
+				slice: []int{1,2,3},
+			}
+		}
+
 		ns[i] = Node{
-			id:    strconv.FormatInt(int64(i), 10),
-			value: i * 2,
+			id:       i,
+			slice:    []int{1, 2, 3},
+			children: children,
 		}
 	}
-	fmt.Printf("ns: %+v", ns)
+
+	//for _, n := range ns { fmt.Println(n) }
+
+	return
+}
+
+type valueCopy func([]Node) []Node
+
+func TestValueCopy(t *testing.T) {
+	fs := []valueCopy{
+		LoopCopy,
+		LoopCopyPreLen,
+		LoopCopyPreCap,
+		LoopCopyPreLenCap,
+		Copy,
+		CopyPreLenCap,
+		Append,
+		LoopDeepCopy,
+		LoopDeepCopyPointerReceiver,
+	}
+
+	ns := provideNodes()
+	for _, f := range fs {
+		cp := f(ns)
+
+		fName := runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
+		if &ns == &cp {
+			t.Errorf("%s output -> same reference: %p %p", fName, &ns, &cp)
+		}
+
+		if len(ns) != len(cp) {
+			t.Errorf("%s output -> dif size: %d %d", fName, len(ns), len(cp))
+		}
+
+		for i  := range cp {
+			if cp[i].id != ns[i].id {
+				t.Errorf("%s output -> dif element(id) %d %d", fName, ns[i].id, cp[i].id)
+			}
+
+			if &cp[i].slice == &ns[i].slice {
+				t.Errorf("%s output -> same ref(slice) %p %p", fName, &ns[i].slice, &cp[i].slice)
+			}
+
+			if &cp[i].children == &ns[i].children {
+				t.Errorf("%s output -> same ref(children) %p %p", fName, &ns[i].children, &cp[i].children)
+			}
+		}
+	}
 }
 
 func BenchmarkLoop(b *testing.B) {
@@ -26,12 +82,12 @@ func BenchmarkLoop(b *testing.B) {
 	}
 }
 
-func BenchmarkLoopPre(b *testing.B) {
+func BenchmarkLoopPreLen(b *testing.B) {
 	ns := provideNodes()
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		LoopCopyPre(ns)
+		LoopCopyPreLen(ns)
 	}
 }
 
@@ -44,12 +100,30 @@ func BenchmarkLoopPreCap(b *testing.B) {
 	}
 }
 
+func BenchmarkLoopPreLenCap(b *testing.B) {
+	ns := provideNodes()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		LoopCopyPreLenCap(ns)
+	}
+}
+
 func BenchmarkCopy(b *testing.B) {
 	ns := provideNodes()
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		Copy(ns)
+	}
+}
+
+func BenchmarkCopyPreLenCap(b *testing.B) {
+	ns := provideNodes()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CopyPreLenCap(ns)
 	}
 }
 
@@ -62,19 +136,130 @@ func BenchmarkAppend(b *testing.B) {
 	}
 }
 
+func BenchmarkLoopDeepCopy(b *testing.B) {
+	ns := provideNodes()
 
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		LoopDeepCopy(ns)
+	}
+}
+
+func BenchmarkDeepCopyPointerReceiver(b *testing.B) {
+	ns := provideNodes()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		LoopDeepCopyPointerReceiver(ns)
+	}
+}
+
+
+
+func providePointerNodes() (ns []*Node) {
+	ns = make([]*Node, 10)
+	for i := 0; i < 10; i++ {
+		children := make([]Node, 5)
+		for c := range children {
+			children[c] = Node{
+				id:    i,
+				slice: []int{1,2,3},
+			}
+		}
+
+		ns[i] = &Node{
+			id:       i,
+			slice:    []int{1, 2, 3},
+			children: children,
+		}
+	}
+
+	//for _, n := range ns { fmt.Println(n) }
+
+	return
+}
+
+type pointerCopy func([]*Node) []*Node
+
+func TestPointerCopy(t *testing.T) {
+	fs := []pointerCopy{
+		LoopCopyPointer,
+		LoopCopyPreLenPointer,
+		LoopCopyPreCapPointer,
+		LoopCopyPreLenCapPointer,
+		LoopDeepCopyPointer,
+	}
+
+	ns := providePointerNodes()
+	for _, f := range fs {
+		cp := f(ns)
+
+		fName := runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
+		if &ns == &cp {
+			t.Errorf("%s output -> same reference: %p %p", fName, &ns, &cp)
+		}
+
+		if len(ns) != len(cp) {
+			t.Errorf("%s output -> dif size: %d %d", fName, len(ns), len(cp))
+		}
+
+		for i  := range cp {
+			if &cp[i] == &ns[i] {
+				t.Errorf("%s output -> same ref(node) %p %p", fName, &ns[i], &cp[i])
+			}
+
+			if cp[i].id != ns[i].id {
+				t.Errorf("%s output -> dif element(id) %d %d", fName, ns[i].id, cp[i].id)
+			}
+
+			if &cp[i].slice == &ns[i].slice {
+				t.Errorf("%s output -> same ref(slice) %p %p", fName, &ns[i].slice, &cp[i].slice)
+			}
+		}
+	}
+}
 
 func BenchmarkLoopPointer(b *testing.B) {
+	ns := providePointerNodes()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		LoopCopyPointer(ns)
+	}
 }
 
 func BenchmarkLoopPrePointer(b *testing.B) {
+	ns := providePointerNodes()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		LoopCopyPreLenPointer(ns)
+	}
 }
 
-func BenchmarkLoopPreCapPointer(b *testing.B) {	
+func BenchmarkLoopPreCapPointer(b *testing.B) {
+	ns := providePointerNodes()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		LoopCopyPreCapPointer(ns)
+	}
 }
 
-func BenchmarkCopyPointer(b *testing.B) {	
+func BenchmarkLoopPreLenCapPointer(b *testing.B) {
+	ns := providePointerNodes()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		LoopCopyPreLenCapPointer(ns)
+	}
 }
 
-func BenchmarkAppendPointer(b *testing.B) {
+func BenchmarkLoopDeepCopyPointer(b *testing.B) {
+	ns := providePointerNodes()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		LoopDeepCopyPointer(ns)
+	}
 }
